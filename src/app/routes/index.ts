5543fe200e323f9c528b710a564d325f5f8bb4de
Delwar Ahmed <delwar.habib@gmail.com>
2023-09-03 00:58:45 +0600

    Project Initialized

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,14 +1,27 @@
 import express from 'express';
+import { UserRoutes } from '../modules/user/user.route';
 
 const router = express.Router();
 
 const moduleRoutes = [
   // ... routes
   {
-    path: "",
-    routes: ""
-  }
+    path: '/users',
+    routes: UserRoutes,
+  },
+  // {
+  //   path: '/categories',
+  //   routes: CategoryRoutes,
+  // },
+  // {
+  //   path: '/books',
+  //   routes: BookRoutes,
+  // },
+  // {
+  //   path: '/orders',
+  //   routes: OrderRoutes,
+  // },
 ];
 
-moduleRoutes.forEach(route => router.use(route.path, route.route));
+moduleRoutes.forEach(route => router.use(route.path, route.routes));
 export default router;
