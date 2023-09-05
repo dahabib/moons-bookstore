a68a473aaafe9412adf9db341b14e38eccd60eed
Delwar Ahmed <delwar.habib@gmail.com>
2023-09-05 19:16:55 +0600

    Completed the requirements.

--- src/app/modules/book/book.route.ts
+++ src/app/modules/book/book.route.ts
@@ -5,7 +5,11 @@ import { BookController } from './book.controller';
 
 const router = express.Router();
 
-router.post('/create-book', BookController.createBook);
+router.post(
+  '/create-book',
+  auth(ENUM_USER_ROLE.ADMIN),
+  BookController.createBook
+);
 router.get('/', BookController.getAllBooks);
 router.get('/:id', BookController.getSingleBook);
 router.get('/:categoryId/category', BookController.getBooksByCategoryId);
